1726357fe851a0542f526f8da2dffb7e952a654c
Tiago Medeiros <medeiros1990@yahoo.com>
2015-01-15 09:58:27 -0500

    One more site..

--- js/list.js
+++ js/list.js
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License along
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
+	
 */
 
 var clickbaits = [	
@@ -31,7 +31,8 @@ var clickbaits = [
 	"cinemablend.com",
 	"screenrant.com",
 	"news.moviefone.com",
-	"moviefone.com"
+	"moviefone.com",
+	"offbeatbride.com"
 	
 	
 ];
